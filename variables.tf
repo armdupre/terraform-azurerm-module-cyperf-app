6ac92c5d675cd0768b2925597e44ed009f38b5f5
armdupre <a-joseph.dupre@keysight.com>
2024-07-09 09:27:56 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -122,11 +122,13 @@ variable "VmSize" {
 	description = "Category, series and instance specifications associated with the VM"
 	type = string
 	validation {
-		condition = contains([	"Standard_F8s_v2",	"Standard_F16s_v2"
+		condition = contains([	"Standard_F8s_v2",	"Standard_F16s_v2",
+								"Experimental_Boost8", "Experimental_Boost32"
 							], var.VmSize)
 		error_message = <<EOF
 VmSize must be one of the following sizes:
-	Standard_F8s_v2, "Standard_F16s_v2"
+	Standard_F8s_v2, "Standard_F16s_v2",
+	Experimental_Boost8, Experimental_Boost32
 		EOF
 	}
 }
